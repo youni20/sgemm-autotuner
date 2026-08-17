62b76e157610c32f7c5c955a9cd76f1f103008e8
Younus <younusmashoor@gmail.com>
2026-08-17 17:42:36 +0200

    chore: formated both files to llvm style standard

--- include/gemm.h
+++ include/gemm.h
@@ -1,20 +1,19 @@
 #pragma once
 
+#include <concepts>
 #include <iostream>
 #include <vector>
-#include <concepts>
 
 using std::vector;
 
-
 template <typename T>
-requires std::integral<T> || std::floating_point<T>
-vector<T> gemm(vector<T>& x, vector<T>& y){
-    vector<T> final_vector;
+  requires std::integral<T> || std::floating_point<T>
+vector<T> gemm(vector<T> &x, vector<T> &y) {
+  vector<T> final_vector;
 
-    for(std::size_t column_value : x){
-        std::cout << column_value;
-    }
+  for (std::size_t column_value : x) {
+    std::cout << column_value;
+  }
 
-    return final_vector;
+  return final_vector;
 }
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,10 @@
-#include <vector>
 #include "gemm.h"
+#include <vector>
 
-int main(){
-    std::vector<int> x = {2,4,6,8};
-    std::vector<int> y = {3,6,9,12};
+int main() {
+  std::vector<int> x = {2, 4, 6, 8};
+  std::vector<int> y = {3, 6, 9, 12};
 
-    gemm(x, y);
-    return 0;
+  gemm(x, y);
+  return 0;
 }
