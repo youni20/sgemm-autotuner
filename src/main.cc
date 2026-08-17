21a5fbab45ecd551237b062a909c431269bdeaa9
Younus <younusmashoor@gmail.com>
2026-08-17 18:21:05 +0200

    chore: reset to now implement actual vectors to go over

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,9 @@
 #include "gemm.h"
 #include <vector>
 
-int main() {
-  std::vector<int> x = {2, 4, 6, 8};
-  std::vector<int> y = {3, 6, 9, 12};
+using std::vector;
 
-  gemm(x, y);
-  return 0;
+int main() {
+
+    return 0;
 }
