e7135ec04de3f934fa2a1150d874d8e59b06d20e
Younus <younusmashoor@gmail.com>
2026-08-17 17:41:32 +0200

    feat: added type checking to the template the same way as adviced from the book cpp book by bjarne

--- include/gemm.h
+++ include/gemm.h
@@ -1,10 +1,15 @@
 #pragma once
+
 #include <iostream>
 #include <vector>
+#include <concepts>
+
 using std::vector;
 
 
-template <typename T> vector<T> gemm(vector<T>& x, vector<T>& y){
+template <typename T>
+requires std::integral<T> || std::floating_point<T>
+vector<T> gemm(vector<T>& x, vector<T>& y){
     vector<T> final_vector;
 
     for(std::size_t column_value : x){
