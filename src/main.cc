96d96d86f3fb1b194dfd47b0ce8c9ad1c2aae289
Younus <younusmashoor@gmail.com>
2026-08-17 21:15:53 +0200

    chore: added a print function to view the contents of matrix C

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,16 @@
 
 using std::vector;
 
+void print_answer(const vector<vector<int>> &x) {
+  for (vector<int> row : x) {
+    std::cout << "{ ";
+    for (std::size_t j{0u}; j < x.size(); ++j) {
+      std::cout << row[j] << " ";
+    }
+    std::cout << "}" << std::endl;
+  }
+}
+
 int main() {
   vector<vector<int>> matrix_a = {// 2x3
                                   {1, 2, 3},
@@ -17,6 +27,7 @@ int main() {
   // std::cout << matrix_a.size() << std::endl;
 
   vector<vector<int>> matrix_c = gemm(matrix_a, matrix_b);
+  print_answer(matrix_c);
 
   return 0;
 }
